baac8cb117f6147e3ead5cc70b477a25af4e9524
catcosmo <astrangethought@hotmail.com>
2017-12-04 01:00:09 +0100

    day 3 star 1 almost finished

---
+++ 3_1.py
@@ -0,0 +1,45 @@
+rank = 0
+quadEdge = 1
+quadSize = 1
+quadStart = 1
+total = 1
+input =  16 #result 34
+
+
+# 1. calculate distance from center to spiral ring
+while total < input:
+    #calculate size of next spiral ring
+    quadSize = ((quadEdge+2)+quadEdge)*2
+    #define new quadedge length
+    quadEdge = quadEdge + 2
+    #calculate starting nunber for current quad
+    quadStart = total+1
+    #calculate total number of numbers in spiral so far
+    total += quadSize
+    #increment rank
+    rank += 1
+
+# 2. calculate  distance from middle of quadEdge to input
+# write the spiral quad in which the input lies in a list
+quad = range(quadStart,quadStart+quadSize)
+# find out what position in the list the input has
+indexInput = quad.index(input)
+# find four middle points
+jump = quadEdge-1
+mid1 = quadStart + jump/2-1
+mid2 = mid1 + jump
+mid3 = mid2 + jump
+mid4 = mid3 + jump
+midPoints = [mid1, mid2, mid3, mid4]
+print midPoints
+#find middle point nearest value & calculate distance
+distance = 0
+for point in midPoints:
+    newDistance = input - point
+    print distance, newDistance
+    if newDistance < distance:
+        distance = newDistance
+
+# add rank and distance
+manhattenDistance = rank + distance
+print manhattenDistance
