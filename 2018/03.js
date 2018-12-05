82ecc2fc4b6998ce8eeff651637e52b353387901
catcosmo <astrangethought@hotmail.com>
2018-12-06 00:44:23 +0100

    minor changes in claimFabric()

--- 2018/03.js
+++ 2018/03.js
@@ -15,20 +15,20 @@ for (let i = 0; i < 1000; i++) fabricRow.push(-1);
 for (let j = 0; j < 1000; j++) fabric.push(fabricRow);
 
 // calculate overlapping
-let overlapping = 0;
+let overlappingTotal = 0;
 for (let claim of input) {
-  // extract numbers from claim-string to clean list
+  // extract numbers from claim-string to array of numbers
   let claimNums = claim.match(/\d+/g).map(Number);
   // add overlapping inches from this claim
-  overlapping += claimFabric(fabric, claimNums);
+  overlappingTotal += claimFabric(fabric, claimNums);
 }
 
 function claimFabric(fabric, claim) {
   let overlapping = 0;
   // fill claim to fabric
-  // claim = [elveID, leftGap, topGap, length, width]
-  for (let i = claim[1]; i <= claim[1] + claim[3]; i++) {
-    for (let j = claim[2]; j <= claim[2] + claim[4]; j++) {
+  // claim = [elveID, leftGap, topGap, width, height]
+  for (let i = claim[1]; i < claim[1] + claim[3]; i++) {
+    for (let j = claim[2]; j < claim[2] + claim[4]; j++) {
       //check if double claimed -> dont count
       if (fabric[i][j] === -2) continue;
       //check if free -> claim for that elf
@@ -43,5 +43,5 @@ function claimFabric(fabric, claim) {
   return overlapping;
 }
 
-console.log(overlapping);
-console.log(fabric[500][2]);
+console.log(fabric);
+console.log(overlappingTotal);
